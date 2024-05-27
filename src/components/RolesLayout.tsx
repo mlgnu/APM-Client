fcd44c165e3889f5a0651412e6329887910f6964
mlgnu <mohamedtahahelmy@gmail.com>
2024-05-27 19:23:17 +0800

    Configuring routes to the application

---
+++ src/components/RolesLayout.tsx
@@ -0,0 +1,82 @@
+import { NavLink } from "@mantine/core";
+import { IconBell, IconUser } from "@tabler/icons-react";
+import React, { useContext, useState } from "react";
+import { Link, Outlet, Routes } from "react-router-dom";
+import { UserContext, useUserContext } from "../utils/UserContext";
+
+export const RolesLayout = () => {
+  const user = useUserContext();
+  // const user = useContext(UserContext);
+  const role = user.role;
+  console.log(role, "roles layout user");
+
+  const navItemsConfig = [
+    [
+      { icon: IconBell, path: "/", label: "Announcements" },
+      { icon: IconBell, path: "message", label: "Messages" },
+      { icon: IconBell, path: "/sessions/view", label: "View Sessions" },
+    ],
+    [
+      { icon: IconBell, path: "/", label: "Announcements" },
+      { icon: IconBell, path: "message", label: "Message Students" },
+      { icon: IconBell, path: "/sessions", label: "Schedule Session" },
+      { icon: IconBell, path: "/sessions/view", label: "View Sessions" },
+      { icon: IconBell, path: "/dashbaord", label: "Feedback Dashboard" },
+      { icon: IconBell, path: "/activity/make", label: "Propose Activity" },
+    ],
+    [
+      { icon: IconBell, path: "/", label: "Announcements" },
+      { icon: IconBell, path: "/assignment", label: "Assignments" },
+      { icon: IconBell, path: "/assignments/view", label: "View Assignments" },
+    ],
+    [
+      { icon: IconBell, path: "/assignments/view", label: "View Assignments" },
+      { icon: IconBell, path: "/assdashboard", label: "Dashboard" },
+    ],
+  ];
+
+  const navItems = navItemsConfig[role] || [];
+  const [activeRoute, setActiveRoute] = useState(0);
+  const navLinks = navItems.map((item, index) => (
+    <NavLink
+      component={Link}
+      to={item.path}
+      key={item.label}
+      active={index === activeRoute}
+      label={item.label}
+      leftSection={<item.icon size="1rem" stroke={1.5} />}
+      onClick={() => setActiveRoute(index)}
+    ></NavLink>
+  ));
+  return <>{navLinks}</>;
+};
+// export const StudentLayout = () => {
+//   const [activeRoute, setActiveRoute] = useState(0);
+//   const routes = [
+//     { icon: IconBell, label: "Announcements", href: "/" },
+//     { icon: IconUser, label: "Assignment", href: "/assignment" },
+//     { icon: IconUser, label: "Message Advisor", href: "/message" },
+//     { icon: IconUser, label: "Monitor", href: "/monitor" },
+//   ];
+//
+//   const navLinks = routes.map((item, index) => (
+//     <NavLink
+//       component={Link}
+//       to={item.href}
+//       key={item.label}
+//       active={index === activeRoute}
+//       label={item.label}
+//       leftSection={<item.icon size="1rem" stroke={1.5} />}
+//       onClick={() => setActiveRoute(index)}
+//     ></NavLink>
+//   ));
+//   return <>{navLinks}</>;
+// };
+//
+// export const AdvisorLayout = () => {
+//   return (
+//     <>
+//       <Link to="/advisor">Advisor</Link>
+//     </>
+//   );
+// };
---
+++ src/components/RolesRoutes.tsx
@@ -0,0 +1,250 @@
+import { useContext } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Assignment } from "../pages/Assignments/Assignment";
+import { ViewAssignments } from "../pages/Assignments/ViewAssignments";
+import { Announcements } from "../pages/Announcements/Announcements";
+import { CollapsedAppShell } from "./AppShell";
+import { MessageAdvisor } from "../pages/Messages/MessageAdvisor";
+import { ManageSession } from "../pages/Monitor/ManageSession";
+import { ViewSessions } from "../pages/Monitor/ViewSessions";
+import { UserContext } from "../utils/UserContext";
+import { AssDashboard } from "../pages/AssDashboard/AssDashboard";
+import FeedbackDashboard from "../pages/MonitorFeebBack/FeedbackDashboard";
+import { ProposeActivity } from "../pages/Activity/ProposeActivity";
+
+const routesConfig = [
+  [
+    { path: "/", element: <Announcements isEditor={false} /> },
+    { path: "message", element: <MessageAdvisor isAdvisor={false} /> },
+    { path: "/sessions/view", element: <ViewSessions isAdvisor={false} /> },
+    { path: "*", element: <div>404</div> },
+  ],
+  [
+    { path: "/", element: <Announcements isEditor={false} /> },
+    { path: "message", element: <MessageAdvisor isAdvisor={true} /> },
+    { path: "/sessions", element: <ManageSession /> },
+    { path: "/sessions/view", element: <ViewSessions isAdvisor={true} /> },
+    { path: "/dashbaord", element: <FeedbackDashboard /> },
+    { path: "activity/make", element: <ProposeActivity /> },
+  ],
+  [
+    { path: "/", element: <Announcements isEditor={true} /> },
+    { path: "/assignment", element: <Assignment /> },
+    {
+      path: "/assignments/view",
+      element: <ViewAssignments isSupervisor={false} />,
+    },
+  ],
+  [
+    { path: "/", element: <Announcements isEditor={false} /> },
+    {
+      path: "/assignments/view",
+      element: <ViewAssignments isSupervisor={true} />,
+    },
+    { path: "/assdashboard", element: <AssDashboard /> },
+  ],
+  [{ path: "/", element: <Announcements isEditor={false} /> }],
+];
+
+export const RoleRoutes = () => {
+  const user = useContext(UserContext);
+  const role = user ? user.role : 4;
+  console.log(role, "roles routes role");
+  const selectedRoutes = routesConfig[role];
+
+  return (
+    <Routes>
+      <Route path="/" element={<CollapsedAppShell />}>
+        {selectedRoutes.map((route, index) => (
+          <Route key={index} path={route.path} element={route.element} />
+        ))}
+      </Route>
+    </Routes>
+  );
+};
+{
+  /* export const StudentRoutes = (props: {}) => { */
+}
+{
+  /*   return ( */
+}
+{
+  /*     <Routes> */
+}
+{
+  /*       <Route path="/" element={<CollapsedAppShell />}> */
+}
+{
+  /*         <Route index element={<Announcements isEditor={false} />} /> */
+}
+{
+  /*         <Route path="message" element={<MessageAdvisor />} /> */
+}
+{
+  /*         <Route path="/monitor" element={<ManageSession />} /> */
+}
+{
+  /*         <Route */
+}
+{
+  /*           path="/sessions/view" */
+}
+{
+  /*           element={<ViewSessions isAdvisor={true} />} */
+}
+{
+  /*         /> */
+}
+{
+  /**/
+}
+{
+  /*         <Route path="*" element={<div>404</div>} /> */
+}
+{
+  /*       </Route> */
+}
+{
+  /*     </Routes> */
+}
+{
+  /*   ); */
+}
+{
+  /* }; */
+}
+{
+  /**/
+}
+{
+  /* export const AdvisorRoutes = (props: {}) => { */
+}
+{
+  /*   return ( */
+}
+{
+  /*     <Routes> */
+}
+{
+  /*       <Route path="/" element={<CollapsedAppShell />}> */
+}
+{
+  /*         <Route path="/assignment" element={<Assignment />} /> */
+}
+{
+  /*         <Route path="message" element={<MessageAdvisor />} /> */
+}
+{
+  /*         <Route path="/monitor" element={<ManageSession />} /> */
+}
+{
+  /*       </Route> */
+}
+{
+  /*     </Routes> */
+}
+{
+  /*   ); */
+}
+{
+  /* }; */
+}
+{
+  /**/
+}
+{
+  /* export const CoordinatorRoutes = (props: {}) => { */
+}
+{
+  /*   return ( */
+}
+{
+  /*     <Routes> */
+}
+{
+  /*       <Route path="/" element={<CollapsedAppShell />}> */
+}
+{
+  /*         <Route path="/assignment" element={<Assignment />} /> */
+}
+{
+  /*         <Route path="/assignments/view" element={<ViewAssignments />} /> */
+}
+{
+  /*       </Route> */
+}
+{
+  /*     </Routes> */
+}
+{
+  /*   ); */
+}
+{
+  /* }; */
+}
+{
+  /**/
+}
+{
+  /* export const SupervisorRoutes = (props: {}) => { */
+}
+{
+  /*   return ( */
+}
+{
+  /*     <Routes> */
+}
+{
+  /*       <Route path="/" element={<CollapsedAppShell />}> */
+}
+{
+  /*         <Route path="/assignments/view" element={<ViewAssignments />} /> */
+}
+{
+  /*       </Route> */
+}
+{
+  /*     </Routes> */
+}
+{
+  /*   ); */
+}
+{
+  /* }; */
+}
+{
+  /**/
+}
+{
+  /* export const RolesRoutes = (props: {}) => { */
+}
+{
+  /*   const user = useContext(UserContext); */
+}
+{
+  /*   return ( */
+}
+{
+  /*     <> */
+}
+{
+  /*       {user?.role === 0 && <StudentRoutes />} */
+}
+{
+  /*       {user?.role === 1 && <AdvisorRoutes />} */
+}
+{
+  /*       {user?.role === 2 && <CoordinatorRoutes />} */
+}
+{
+  /*       {user?.role === 3 && <SupervisorRoutes />} */
+}
+{
+  /*     </> */
+}
+{
+  /*   ); */
+}
+{
+  /* }; */
+}
